55fc81a6581c1290d6f266a24c9905a50b750474
Matheus Monte <freitas.monte9@gmail.com>
2025-10-19 12:15:30 -0300

    adding postgresql as DW

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -2,6 +2,6 @@ import gdown
 
 url = "https://drive.google.com/drive/folders/1gLSw0RLjBbtaNy0dgnGQDAZOHIgCe-HH"
 output = "../data/raw"
-my_user_agente = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0'
+my_user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:143.0) Gecko/20100101 Firefox/143.0'
 
-gdown.download_folder(url=url, output=output, user_agent=my_user_agente)
+gdown.download_folder(url=url, output=output, user_agent=my_user_agent)
